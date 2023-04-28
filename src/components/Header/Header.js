34dc24d83595445f2aa7d715b0f0f30d673dce44
aklaran <botbunk@gmail.com>
2023-04-28 10:39:06 -0400

    responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES, COLORS, WEIGHTS } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,23 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopButtonGroup>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </DesktopButtonGroup>
         <Logo />
+        <RightDesktopButtonGroupWrapper>
+          <RightDesktopButtonGroup>
+            <Button>Subscribe</Button>
+            <LoginLink href="">Already a subscriber?</LoginLink>
+          </RightDesktopButtonGroup>
+        </RightDesktopButtonGroupWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +55,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +85,67 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: space-between;
+    align-items: center;
+
+    margin-block-start: 16px;
+    margin-block-end: 72px;
+  }
+`;
+
+const DesktopButtonGroup = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+    flex: 1;
+  }
+`;
+
+const Spacer = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+    flex: 1;
+  }
+`;
+
+const RightDesktopButtonGroupWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-self: last baseline;
+
+    flex: 1;
+  }
+`;
+
+const RightDesktopButtonGroup = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+    align-self: last baseline;
+  }
+`;
+
+const LoginLink = styled.a`
+  font-family: var(--font-family-serif);
+  font-style: italic;
+  text-decoration: underline;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.normal};
+  font-size: ${14 / 16}rem;
+  text-align: center;
 `;
 
 export default Header;
